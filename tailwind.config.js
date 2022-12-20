91a0d873db2d0a38c5984f14a57915360a1f516c
Emre Sezer <emresezer1903@gmail.com>
2022-12-20 16:15:19 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@
 
 module.exports = {
   content:  
-  ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"],
+  ["pages/**/*.{js,jsx,ts,tsx}", "components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
